2384e0a6edf92b19f8255370ce26bfdbceb960d4
Ian <ian@dangerousprototypes.com>
2024-12-11 12:38:44 +0100

    prepare for pirate-lib

--- include/board_buspirate5_config.h
+++ include/board_buspirate5_config.h
@@ -80,6 +80,6 @@
 #define PICOPROBE_UART_RX_LED 7
 #define PICOPROBE_UART_TX_LED 8
 */
-#define PROBE_PRODUCT_STRING "Example Debug Probe"
+#define PROBE_PRODUCT_STRING "Debug Probe (CMSIS-DAP)"
 
 #endif
